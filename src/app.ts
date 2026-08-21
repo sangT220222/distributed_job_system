f84d4c538259ed445e0d2848699c7038cdc41aeb
Sangam <100322380+sangT220222@users.noreply.github.com>
2026-08-21 10:54:21 +0100

    feat: express.js initialised. initial endpoint implemented

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
 // Setting up Express application
+import express from "express";
+import dotenv from "dotenv";
+
+export const app = express();
+dotenv.config();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "TS API running" });
+});
+
+export default app;
