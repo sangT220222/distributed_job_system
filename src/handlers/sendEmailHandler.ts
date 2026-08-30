cfd3d7c9f5a3d7d82dc8be52b6c8bb571a178eed
Sangam <100322380+sangT220222@users.noreply.github.com>
2026-08-30 09:56:14 +0100

    feat: repository and handler functions implemented, basic single worker initialsed

--- src/handlers/sendEmailHandler.ts
+++ src/handlers/sendEmailHandler.ts
@@ -1,3 +1,11 @@
-export function emailHandler(data: object) {
-  //send email with the data -> make a payload so it's sendable?
+type JobDataContent = {
+  email_address: string;
+  email_data: string;
+};
+
+export async function emailHandler(data: JobDataContent) {
+  //placeholder - mimic an email sending process
+  console.log(`sending email to ${data.email_address}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  console.log(`Email sent to ${data.email_address}`);
 }
--- src/workers/worker.ts
+++ src/workers/worker.ts
@@ -1,27 +1,43 @@
 //worker to call SQL to retrieve oldest job that's stand_by
 //call SQL query to update job_status and finished_at
 //polling method to see if next job avaliable
-
 import { emailHandler } from "../handlers/sendEmailHandler.js";
 import { retrieveJob } from "../repositories/retrieveJob.js";
-import { updateJobStatus } from "../repositories/updateJob.js";
+import { markJobFinished, markJobStarted } from "../repositories/updateJob.js";
 
 async function startWorker() {
-  const job_payload = await retrieveJob();
-  if (job_payload === null) {
-    return;
-  }
-  const job_id = job_payload.id;
-  //update status to started
-  await updateJobStatus("started", job_id);
-  //   const job_type = job_payload.job_type;
-  //   const job_data = job_payload.job_data;
+  //polling
+  while (true) {
+    const job_payload = await retrieveJob();
+    if (job_payload === null) {
+      //wait for two secs
+      console.log("No jobs avaliable. Checking again in 2 seconds");
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      continue;
+    }
+    const job_id = job_payload.id;
+    //update status to started
+    await markJobStarted("started", job_id);
+    const job_type = job_payload.job_type;
+    const job_data = job_payload.job_data;
 
-  //calls the correct handler based on job_type, and execute the right job logic
-  // await jobHandler(job_type, job_data);
-  //   if (job_type === "send_email") {
-  //     emailHandler(job_data);
-  //   }
+    try {
+      //as we add more handlers, switch case to be used rather that if & else
+      if (job_type === "send_email") {
+        await emailHandler(job_data);
+        //call repo to update the DB for status = completed, finished_at = currentTime
+        await markJobFinished("completed", job_id);
+      }
+      //else if other job_type
+      else {
+        throw new Error(`Unsupported job type : ${job_type}`);
+      }
+    } catch (error) {
+      //call repo to update the DB for status = failed, finished_at = currentTime
+      console.error("Job failed ", error);
+      await markJobFinished("failed", job_id);
+    }
+  }
 }
 
 startWorker();
